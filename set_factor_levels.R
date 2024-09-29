eb9b839b7e47e7ed6ca34ceac57b5a2643c84e18
David Pressley <david@clearcreekanalytics.com>
2024-09-29 17:48:34 -0400

    rename commit (set_factor_levels)

--- set_factor_levels.R
+++ set_factor_levels.R
@@ -1,14 +1,23 @@
+library(httr)
 #get factor levels
-get_pums_factor_levels <- function(var_name){
+get_pums_factor_levels <- function(var_names){
+  #initialize list
+  levels_list <- list()
   #get base URL
-  url <- paste0("https://api.census.gov/data/2022/acs/acs1/pums/variables/",var_name,".json")
-  response <- GET(url)
+  for (var_name in var_names){
+    url <- paste0("https://api.census.gov/data/2022/acs/acs1/pums/variables/",var_name,".json")
+    response <- GET(url)
+    if (status_code(response) == 200) {
+      data <- fromJSON(content(response,"text"))
+      levels <- data$values$item
+      levels_list[[var_name]] <- levels
+    }
+  }
   
-  data <- fromJSON(content(response,"text"))
-  return(data$values$item)
+  return(levels_list)
   # return(var_name$values$item)
 }
 
-x <- get_pums_factor_levels("SEX")
+x <- get_pums_factor_levels(c("SEX","PWGTP"))
 
 # https://api.census.gov/data/2022/acs/acs1/pums/variables.json
