2a81366c4aec7e89594c4634eda1306e84cf2932
David Pressley <david@clearcreekanalytics.com>
2024-09-30 17:47:38 -0400

    clean up get_PUMS. working code.

--- get_pums.R
+++ get_pums.R
@@ -1,3 +1,6 @@
+library(dplyr)
+library(readr)
+library(tidyverse)
 library(tidycensus)
 library(jsonlite)
 library(httr)
@@ -5,66 +8,55 @@ library(httr)
 #store key
 census_api_key("bdb1f6ff2e4982a1da64cd526532aa92dca5581c", install = TRUE, overwrite = TRUE)
 
-#from Project 1 documentation
-# pums_URL <- "https://api.census.gov/data/2022/acs/acs1/pums?get=SEX,PWGTP,MAR&for=state:37&SCHL=24"
 
-# From Lesson
-# test_PUMS_GET <- httr::GET(pums_URL)
-# str(test_PUMS_GET, max.level = 1)
-# parsed_PUMS <- fromJSON(rawToChar(test_PUMS_GET$content))
-# test_PUMS_tibble <- as_tibble(parsed_PUMS)
-# test_PUMS_tibble |>
-#   select(everything())
-
-
-get_PUMS <- function(geography, optional_vars, key=sys.getenv("CENSUS_API_KEY"), year=2022,
-                     state = "*",county = NULL, zcta = NULL, survey="acs1", show_call = FALSE){
+get_PUMS <- function(geography, user_vars, key=sys.getenv("CENSUS_API_KEY"), 
+                             year=2022, state = "*",county = NULL, zcta = NULL, 
+                             survey="acs1", show_call = FALSE)
+{
+  #get check_vars metadata
+  var_check <- check_vars(geography,year)
+  # If check_vars returns a message (missing vars), stop the function
+  if (!isTRUE(var_check)) {
+    stop(var_check)  # Stops and prints the error message from check_vars
+  }
+  
   #get base URL
   baseURL <- paste("https://api.census.gov/data",
-                      year, "acs",
-                      survey, "pums", sep = "/")
+                   as.character(year), "acs",
+                   survey, "pums", sep = "/")
   
   #get parameters from function args
-  paramsURL <- paste0("?get=", optional_vars, "&for=", geography, ":", state, "&key=", key)
+  paramsURL <- paste0("?get=", user_vars, "&for=", geography, ":", state, "&key=", key)
   apiURL <- (paste0(baseURL,paramsURL))
   
   #check
   print(apiURL)
-
+  
   response <- httr::GET(apiURL)
   
-  #Data Checks
-  if (status_code(response) == 200){
-    allowed_vars <- c("PWGTP","MAR","AGEP", "GASP", "GRPIP", "JWAP", "JWDP", "JWMNP","FER","HHL","HISPEED","JWAP","JWDP","JWTRNS","SCH","SCHL","SEX")
-    optional_vars <- unlist(strsplit(optional_vars, ","))
-    if (!all(optional_vars %in% allowed_vars)) {
-      stop("User provided variable list only allows for variables: PWGTP,MAR,AGEP,GASP,GRPIP,JWAP,JWDP,JWMNP,FER,HHL,HISPEED,JWAP,JWDP,JWTRNS,SCH,SCHL,and SEX")
-    }
-    else{
-      
-      parsed_data <- as_tibble(fromJSON(rawToChar(response$content)))
-
-      #set column names from first row
-      col_names <- parsed_data[1,]
-      parsed_data <- parsed_data[-1,]
-      parsed_data <- setNames(parsed_data, col_names) 
-      return(parsed_data)
-    }
-  }
-  else {
-    stop("Something wrong with query: ", status_code(response))
-  }
-
+  # Convert API response to a tibble |>
+  #set factor levels and convert data types
+  var_metadata <- get_var_metadata(user_vars)
+  parsed_data <- return_tibble(response$content) 
+  # |> 
+  #   clean_names()
+  
+  #return a list with two tibbles
+  api_data <- list(api_data = parsed_data, api_metadata = var_metadata)
+  
+  # Set factor levels and types for the tibble
+  parsed_data <- set_factor_levels_types(parsed_data, var_metadata)
+  return(api_data)
 }
 
-
 #Test-9001
+
 geography <- "state"
-user_vars <- "SEX,PWGTP,MAR,HISPEED"
+user_vars <- "SEX,FER,HHL,PWGTP,AGEP,GASP,GRPIP,JWAP,JWDP,JWMNP"
+# user_vars <- "SEX,PWGTP,MAR,HISPEED"
 key <- "bdb1f6ff2e4982a1da64cd526532aa92dca5581c"
-state <- "05"  # 
+state <- "05"  # Arkansas
 year <- 2022
 
-pums_data <- get_PUMS(geography,user_vars,key,year,state)
+pums_data <- get_PUMS_minimal(geography,user_vars,key,year,state)
 print(pums_data)
-
