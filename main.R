dc6e4bd57a668e248e97561d3f2058955ef4c171
David Pressley <david@clearcreekanalytics.com>
2024-10-01 14:29:30 -0400

    update driver function

--- main.R
+++ main.R
@@ -11,12 +11,17 @@ source("factor_levels.R")
 source("get_var_metadata.R")
 
 
+xnum_vars <- "GASP"
+# xnum_vars <- NULL
+# xchr_vars <- "HISPEED"
+xchr_vars <- NULL
 geography <- "state"
-user_vars <- "SEX,FER,HHL,PWGTP,AGEP,GASP,GRPIP,JWAP,JWDP,JWMNP"
+# user_vars <- "SEX,FER,HHL,PWGTP,AGEP,GASP,GRPIP,JWAP,JWDP,JWMNP"
 # user_vars <- "SEX,PWGTP,MAR,HISPEED"
+# user_vars <- "SEX"
 key <- "bdb1f6ff2e4982a1da64cd526532aa92dca5581c"
 state <- "05"  # Arkansas
 year <- 2022
 
-pums_data <- get_PUMS_minimal(geography,user_vars,key,year,state)
+pums_data <- get_PUMS(geography,xnum_vars,xchr_vars,key,year,state)
 print(pums_data)
