de26690a677a8af0d4e0188c4edc4c7557ca3c53
David Pressley <david@clearcreekanalytics.com>
2024-10-01 14:26:11 -0400

    update check_vars to handle missing data from 2020

--- check_vars.R
+++ check_vars.R
@@ -3,10 +3,15 @@ check_vars <- function(geography, year)
 {
   year_range <- seq(2010,2022, by = 1)
   if (!geography %in% c("all","region","division","state")){
-    stop(paste(geography), "is not an allowed value for the geography variable. Options are: all, region, division, state")
-  } else if (!year %in% year_range){
+    stop(paste(geography), " is not an allowed value for the geography variable. Options are: all, region, division, state")
+  } 
+  if (!year %in% year_range){
     stop(paste(year), " is not an allowed value for the year variable. Options are between 2010 - 2022")
-  } else{
+  } 
+  else if (year == 2020){
+    stop(paste(year), " is not available from census.gov")
+  }
+  else{
     return(TRUE)
   }
 }
