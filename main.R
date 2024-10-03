1abaca8bf0ecb966c88ee58abc4ef099d1748661
David Pressley <david@clearcreekanalytics.com>
2024-10-03 15:24:40 -0400

    update driver function main.R. added library and rearranged order of sourcing

--- main.R
+++ main.R
@@ -1,20 +1,24 @@
-library(dplyr)
 library(readr)
-library(tidyverse)
 library(tidycensus)
 library(jsonlite)
 library(httr)
+library(stringr)
+library(lubridate)
+library(janitor)
 source("return_tibble.R")
-source("get_pums.R")
 source("check_vars.R")
 source("factor_levels.R")
 source("get_var_metadata.R")
+source("extract_times.R")
+
+library(tidyverse)
+source("get_pums.R")
 
 
-xnum_vars <- "GASP"
+xnum_vars <- "PWGTP,GASP"
 # xnum_vars <- NULL
 # xchr_vars <- "HISPEED"
-xchr_vars <- NULL
+xchr_vars <- "SEX,JWAP,JWDP,JWMNP"
 geography <- "state"
 # user_vars <- "SEX,FER,HHL,PWGTP,AGEP,GASP,GRPIP,JWAP,JWDP,JWMNP"
 # user_vars <- "SEX,PWGTP,MAR,HISPEED"
@@ -25,3 +29,4 @@ year <- 2022
 
 pums_data <- get_PUMS(geography,xnum_vars,xchr_vars,key,year,state)
 print(pums_data)
+print(unique(pums_data$api_data$JWAP))
